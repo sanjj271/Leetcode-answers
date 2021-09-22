903549f79fa22881b72cbd275dcf2e84c44a0372
Sanjana gogte <sanj27272@gmail.com>
2021-09-22 11:47:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.29%) - LeetHub

---
+++ sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+    for(long i=0;i*i<=c;i++){
+        double x=sqrt(c-(i*i));
+        if(x==int(x)){
+            return true;
+        }
+    }
+    return false;
+        
+        
+        
+    }
+    
+};
