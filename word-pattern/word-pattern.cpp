5a8dba2d1b3b660517fd7444b0f8dfa88117113c
Sanjana gogte <sanj27272@gmail.com>
2021-10-05 18:09:21 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (81.62%) - LeetHub

--- word-pattern/word-pattern.cpp
+++ word-pattern/word-pattern.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        map<char, string> mp;
-        map<string,char>m;
-        map<char,string>::iterator it;
-         map<string,char>::iterator it1;
-        vector<string> v1;
-        stringstream ss(s);
-        string token;
-        
-        while(getline(ss, token, ' ')){
-            v1.push_back(token);
-        }
-        if(v1.size()!=pattern.length()){
-            return false;
-        }
-        for(int i=0;i<pattern.size();i++){
-            it = mp.find(pattern[i]);
-          if (it != mp.end()){
-             if( it->second!=v1[i]){
-                 return false;
-             }
-          }
-            else{
-                mp.insert({pattern[i],v1[i]});
-                it1 = m.find(v1[i]);
-                if (it1 != m.end()){
-                    return false;
-                }
-                else{
-                    m.insert({v1[i],pattern[i]});
-                }
-            
-            }
-            
-              
-        }
-        return true;
+    map<char,string> mp;
+    map<string,char> mpp;
+    vector<string> v;
+    //getline(cin,s);
+    istringstream iss(s);
+    string word;
+    while(iss >> word) {
+        v.push_back(word);
+   
     }
+    if(pattern.length()!=v.size()){
+        return false;
+    }
+    for(int i=0;i<pattern.length();i++){
+            if(mp.find(pattern[i])!=mp.end()){
+                if(mp[pattern[i]]!=v[i]){
+                    return false;
+                }        
+            }
+            else{
+                mp[pattern[i]]=v[i];
+                
+            }
+        
+        
+        
+    }
+    for(int i=0;i<v.size();i++){
+            if(mpp.find(v[i])!=mpp.end()){
+                if(mpp[v[i]]!=pattern[i]){
+                    return false;
+                }        
+            }
+            else{
+                mpp[v[i]]=pattern[i];
+                
+            }
+    }
+    return true;
+    }
+        
+            
+    
 };
