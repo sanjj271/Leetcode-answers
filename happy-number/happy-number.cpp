4310780caab669d08a0804c5996fcb95e500db31
Sanjana gogte <sanj27272@gmail.com>
2021-10-20 23:16:51 +0530

    Time: 4 ms (43.30%), Space: 6.3 MB (28.04%) - LeetHub

--- happy-number/happy-number.cpp
+++ happy-number/happy-number.cpp
@@ -3,18 +3,22 @@ public:
     bool isHappy(int n) {
         set<int> s;
         int sum=0;
+        // Split the number into digits using stringstream
         stringstream ss;
         ss << n;
         string str = ss.str();
         for(int i=0;i<str.size();i++){
             sum=sum+((str[i]-'0')*(str[i]-'0'));
             }
+        //check if sum is present in the set
         if(s.find(sum) != s.end()){
             return false;
         }
+        //if sum is not present, add it to the set
         else{
             s.insert(sum);
         }
+        //repeat the previous procedure for sum
         while(true){
         stringstream ss;
         ss << sum;
@@ -24,11 +28,12 @@ public:
             sum=sum+((str[i]-'0')*(str[i]-'0'));
             
         }
+        //check if sum is a happy number 
         if(sum==1){
             return true;
         }
         else{
-            
+            //Return false if the number is not a happy number 
             if (s.find(sum) != s.end()){
                 return false;
             }
