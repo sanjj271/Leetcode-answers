bc3bc9dda36f3a3be67f8a5dc4da0f896aa56443
Sanjana gogte <sanj27272@gmail.com>
2021-11-19 12:18:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (10.87%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
+    map<int,int>mp;
     int fib(int n) {
+      //map<int,int>mp;
       if(n==0 || n==1){
           return n;
       }
-      return fib(n - 1) + fib(n - 2);
+      if(mp.find(n)==mp.end()){
+          mp[n] = fib(n-1) + fib(n-2);
+      }
+    return mp[n];
+         
+    
+
+         
+        
+
+      
+     
+        
         
         
         
