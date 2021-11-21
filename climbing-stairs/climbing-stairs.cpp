b88a87904945b7030f8243e0fcceb827a28e13f8
Sanjana gogte <sanj27272@gmail.com>
2021-11-21 14:57:02 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (26.07%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    map<int,int>mp;
+    int climbStairs(int n) {  
+    if(n == 0){
+        return 1;
+    }
+    if(n == 1 ){
+        return 1;
+    }
+    if(mp.find(n)==mp.end()){
+    mp[n] = climbStairs(n-1) + climbStairs(n-2);
+    }
+    return mp[n];
+        
+        
+    }
+};
