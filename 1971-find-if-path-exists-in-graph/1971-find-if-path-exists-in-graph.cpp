a9fdc5b52a7d09e57abdfdd5f9c9c34c7f3fbb7d
Sanjana gogte <sanj27272@gmail.com>
2022-02-01 14:14:31 +0530

    Time: 1043 ms (34.32%), Space: 164.6 MB (62.79%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
-    //map<int , vector<int>>mp;
-    //vector<bool>visited(n,false);
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>> mp(n,vector<int>());
-        vector<bool>visited(n,false);
-        for(int i = 0 ; i<edges.size();i++){
-            mp[edges[i][0]].push_back(edges[i][1]);
-            mp[edges[i][1]].push_back(edges[i][0]);
-        }
-        return dfs(mp,visited,source,destination);
-        
-        
-        
-           
+    //map<int,vector<int>>mp;
+    vector<vector<int>> mp(n,vector<int>());
+    vector<bool>visited(n,false);
+    for(int k = 0 ; k < edges.size();k++){
+        mp[edges[k][0]].push_back(edges[k][1]);
+        mp[edges[k][1]].push_back(edges[k][0]);    
     }
-    bool dfs(vector<vector<int>>&mp, vector<bool>&visited , int start , int end){   
+    return dfs(mp,visited,source,destination);
+        
+        
+    }
+    bool dfs(vector<vector<int>>&mp , vector<bool>&visited , int start , int end){
         if(start == end){
             return true;
         }
-       // if(visited[start]) return false;
-        //if(mp[start].size()==0){
-          // return false; 
-        //} 
         visited[start]=true;
-        for(int j = 0 ;j < mp[start].size();j++){
+        for(int j = 0 ; j<mp[start].size();j++){
             if(visited[mp[start][j]]==false){
-            
-            if(dfs(mp,visited,mp[start][j],end)){
-                return true;
+                if(dfs(mp,visited,mp[start][j],end)){
+                    return true;
+                }
             }
-            }
-           
         }
         return false;
-        
-        
     }
-    
-    
-    
 };
