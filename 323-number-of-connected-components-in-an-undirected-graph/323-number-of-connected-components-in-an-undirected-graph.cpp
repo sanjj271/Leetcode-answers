8b2bd788989234437817e4989147f0898230ac35
Sanjana gogte <sanj27272@gmail.com>
2022-02-01 13:05:33 +0530

    Time: 26 ms (53.69%), Space: 13.6 MB (42.10%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> mp(n,vector<int>());
+        vector<bool>visited(n,false);
+        int count =0;
+        for(int i = 0 ; i<edges.size();i++){
+            mp[edges[i][0]].push_back(edges[i][1]);
+            mp[edges[i][1]].push_back(edges[i][0]);
+        }
+        for(int i =0;i<n ;i++){
+            if(visited[i]==false){
+               count++;
+               dfs(mp,visited,i);
+            }
+        }
+        return count;
+            
+            
+            
+        
+    
+    }
+    void dfs(vector<vector<int>>&mp , vector<bool>&visited , int src){
+        visited[src] = true;
+        
+        for(int j = 0 ;j < mp[src].size();j++){
+            if(visited[mp[src][j]]==false){
+                dfs(mp,visited,mp[src][j]);
+            }
+        
+        }
+    }
+};
