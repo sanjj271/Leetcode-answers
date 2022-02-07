86aec46c375f690b4146bb1f4e92da19cb68bf85
Sanjana gogte <sanj27272@gmail.com>
2022-02-07 13:50:51 +0530

    Time: 43 ms (54.65%), Space: 71.5 MB (49.40%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,99 +11,96 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    int sum =0;
     int carry = 0;
-    int sum = 0;
-    ListNode* prev ;
-    ListNode* curr;
     ListNode* head = NULL;
-    while(l1!= NULL && l2!=NULL){
-        ListNode* finall = new ListNode();
+    ListNode* prev;
+    ListNode* curr;
+    while(l1!= NULL & l2!=NULL){
         sum =0;
-        sum=l1->val + l2->val + carry;
+        ListNode* nptr = new ListNode;
+        sum = l1->val + l2->val + carry;
         l1 = l1->next;
-        l2 = l2->next;
-        if(sum >= 10){
+        l2= l2->next;
+        if(sum >= 10 ){
             carry = 1;
         }
         else{
             carry = 0;
         }
-        finall->val = sum % 10;
+        nptr->val = sum%10;
         if(head == NULL){
-            head = finall;
+            head = nptr;
             prev = head;
+    
         }
         else{
-            curr = finall;
+            curr= nptr;
             prev -> next = curr;
-            prev = curr;       
-        }   
+            prev = curr;   
+        }
+
     }
-    while(l1!=NULL){
-        ListNode* finall = new ListNode();
-        sum = 0;
-        sum=l1->val +  carry;
+    while(l1!= NULL){
+        sum =0;
+        ListNode* nptr = new ListNode;
+        sum = l1->val  + carry;
         l1 = l1->next;
-        if(sum >= 10){
+        if(sum >= 10 ){
             carry = 1;
         }
         else{
             carry = 0;
         }
-        finall->val = sum % 10;
+        nptr->val = sum%10;
         if(head == NULL){
-            head = finall;
+            head = nptr;
             prev = head;
+    
         }
         else{
-            curr = finall;
+            curr= nptr;
             prev -> next = curr;
-            prev = curr;       
-        }   
-        
+            prev = curr;   
+        }
+
     }
     while(l2!=NULL){
-        ListNode* finall = new ListNode();
-        sum = 0;
-        sum=l2->val +  carry;
-        l2 = l2->next;
-        if(sum >= 10){
+        sum =0;
+        ListNode* nptr = new ListNode;
+        sum =  l2->val + carry;
+        l2= l2->next;
+        if(sum >= 10 ){
             carry = 1;
         }
         else{
             carry = 0;
         }
-        finall->val = sum % 10;
+        nptr->val = sum%10;
         if(head == NULL){
-            head = finall;
+            head = nptr;
             prev = head;
+    
         }
         else{
-            curr = finall;
+            curr= nptr;
             prev -> next = curr;
-            prev = curr;       
-        }   
-        
+            prev = curr;   
+        }
+
     }
-    if(carry == 1){
-      ListNode* finall = new ListNode();
-      curr = finall;
-      finall -> val = 1;
-      prev -> next = curr;
-      prev = curr;   
+    if(carry == 1 ){
+        ListNode* nptr = new ListNode;
+        curr= nptr;
+        nptr->val = 1;
+        prev -> next = curr;
+        prev = curr;
+        
     }
     return head;
     
-    
-        
-    
-        
-        
-    
-        
         
         
         
     }
-    
 };
