9d0a929e362114c2c53d57b523babced159c2e23
Sanjana gogte <sanj27272@gmail.com>
2022-02-08 16:19:06 +0530

    Time: 20 ms (65.92%), Space: 11.6 MB (93.76%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end());
+    if(intervals.size() == 0){
+        return true;
+    }
+    for(int i =0;i<intervals.size()-1;i++){
+        if(intervals[i][1] > intervals[i+1][0]){
+            return false;
+        }
+
+    }
+    return true;
+        
+    }
+};
