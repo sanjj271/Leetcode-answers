d232cfdd3856373f12f7201a35354374e4c15890
Sanjana gogte <sanj27272@gmail.com>
2022-02-28 08:07:32 +0530

    Time: 68 ms (50.13%), Space: 368.6 MB (12.15%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -4,7 +4,27 @@ public:
     if(needle == ""){
         return 0;
     }
-    return haystack.find(needle);
+    if(haystack==""){
+        return -1;
+    }
+    if(haystack == needle){
+        return 0;
+    }
+    if(needle.size()>haystack.size()){
+        return -1;
+    }
+    int k = needle.size();
+    for(int i =0;i<=haystack.size()-k;i++){
+    string comp = haystack.substr(i,k);
+    if(comp == needle){
+        return i;   
+    }
+    }
+    return -1;
+        
+   
+   
+    
         
     }
 };
