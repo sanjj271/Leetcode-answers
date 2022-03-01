84b26bda571d8d47a1187d187c567bcf19e84964
Sanjana gogte <sanj27272@gmail.com>
2022-03-01 07:34:25 +0530

    Time: 4 ms (54.77%), Space: 5.7 MB (99.98%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        
+    if (dividend == INT_MIN && divisor == -1) {
+        return INT_MAX;
+    }
+    int s = dividend/divisor;
+    return trunc(s);
+    };
+    
+};
