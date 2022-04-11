088e484fb73cde4c03884d5d55320813a3abd4f4
Sanjana gogte <sanj27272@gmail.com>
2022-04-11 06:24:38 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (91.09%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+    map<int,int>mp;
+    for(int i =0;i<nums.size();i++){
+        mp[nums[i]]++;
+    }
+    nums.clear();
+    for(auto j : mp){
+        for(int i = 0; i < j.second;i++){
+            nums.push_back(j.first);    
+        }
+    }
+        
         
     }
 };
