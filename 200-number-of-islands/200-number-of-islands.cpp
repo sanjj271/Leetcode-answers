0e18bc520294f4d92382eb6c2adeda32e7a98dfa
Sanjana gogte <sanj27272@gmail.com>
2022-06-07 12:35:50 +0530

    Time: 41 ms (73.24%), Space: 12.3 MB (84.92%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int r, int c) {
+    int numIslands(vector<vector<char>>& grid) {
+    int nr =grid.size();
+    int nc=grid[0].size();
+    int count =0;
+     for(int i =0;i<nr;i++){
+         for(int j = 0;j<nc;j++){
+             if(grid[i][j]=='1'){
+                 count++;
+                 DFS(grid,i,j);
+             }
+         }
+     }
+    return count;
+      
+    }
+    void DFS(vector<vector<char>>& grid, int row, int col){
     int nr = grid.size();
     int nc = grid[0].size();
     
-    grid[r][c]='0';
-    if(r+1 <nr && grid[r+1][c] == '1'){
-        dfs(grid,r+1,c);  
+    grid[row][col]='0';
+    if(row+1<nr && grid[row+1][col]=='1'){
+        DFS(grid,row+1,col);
     }
-    if(c+1 <nc && grid[r][c+1] == '1'){
-        dfs(grid,r,c+1);  
+    if(col+1<nc && grid[row][col+1]=='1'){
+        DFS(grid,row,col+1);
     }
-    if(c-1 >=0 && grid[r][c-1] == '1'){
-        dfs(grid,r,c-1);  
+    if(col-1>=0 && grid[row][col-1]=='1'){
+        DFS(grid,row,col-1);
     }
-    if(r-1 >=0 && grid[r-1][c] == '1'){
-        dfs(grid,r-1,c);  
+    if(row-1>=0 && grid[row-1][col]=='1'){
+        DFS(grid,row-1,col);
     }
-        
+    }
     
-        
-    }
-    int numIslands(vector<vector<char>>& grid){
-    int nr = grid.size();
-    int nc = grid[0].size();  
-    int count= 0;
-        
-    for(int r = 0;r<nr;r++){
-        for(int c =0 ; c< nc ;c++){
-            if(grid[r][c]=='1'){
-                count++;
-                dfs(grid,r,c);
-            }
-        }
-    }
-    return count;
-    }
+    
+    
 };
