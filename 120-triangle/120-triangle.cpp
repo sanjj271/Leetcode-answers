b39fea42e9c4ed0ab95dac25d2420bdf0883f543
Sanjana gogte <sanj27272@gmail.com>
2022-06-13 22:57:59 +0530

    Time: 142 ms (5.45%), Space: 13.6 MB (5.41%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,9 +3,6 @@ public:
     map<string,int>mp;
     int minimumTotal(vector<vector<int>>& triangle) {
      return solve(0,0,triangle);
-        
-        
-        
     }
     int solve(int i, int j, vector<vector<int>>& triangle){
        if(i == triangle.size()-1){
@@ -16,7 +13,7 @@ public:
        int b = triangle[i][j] + solve(i+1,j+1, triangle);
         mp[to_string(i)+"-"+to_string(j)]=min(a,b);
        }
-      return mp[to_string(i)+"-"+to_string(j)];
+       return mp[to_string(i)+"-"+to_string(j)];
     }
     
 };
