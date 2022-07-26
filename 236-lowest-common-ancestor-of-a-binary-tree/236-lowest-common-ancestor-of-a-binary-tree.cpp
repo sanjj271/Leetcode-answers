c413a08ae05d5f5ec8d2f645c7abc596dbb6b049
Sanjana gogte <sanj27272@gmail.com>
2022-07-26 07:04:12 +0530

    Time: 2057 ms (5.09%), Space: 14.3 MB (25.30%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(isSubtree(root->left,p) && isSubtree(root->right,q)){
+        cout<<"im here1"<<endl;
+        return root;
+    }
+    if(isSubtree(root->left,p) && isSubtree(root->left,q)){
+        cout<<"im here2"<<endl;
+        return lowestCommonAncestor(root->left,p,q);
+    }
+    if(isSubtree(root->right,p) && isSubtree(root->right,q)){
+        cout<<"im here3"<<endl;
+        return lowestCommonAncestor(root->right,p,q);
+    }
+    if(root->val == p->val){
+         cout<<"im here4"<<endl;
+         if(isSubtree(root->right,q) || root->val == q->val ){
+             return root;
+         }
+    }
+    if(root->val == q->val){
+         if(isSubtree(root->left,p) || root->val == p->val ){
+             return root;
+         }
+    }
+    return root;
+        
+    
+    
+    
+        
+    }
+    bool isSubtree(TreeNode* root,TreeNode* p){
+    if(root==NULL){
+        return false; 
+    }
+    if(root->val==p->val){
+        return true;
+    }
+    return isSubtree(root->left,p) || isSubtree(root->right,p);
+    
+    
+    
+    
+    
+    }
+};
