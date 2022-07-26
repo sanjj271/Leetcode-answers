e2a5a220295df1eb12137f0903cfd79aafc502f7
Sanjana gogte <sanj27272@gmail.com>
2022-07-26 22:19:00 +0530

    Time: 1040 ms (5.00%), Space: 113.4 MB (5.42%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int lsf = INT_MAX;
-        int op = 0;
-        int pist = 0;
-        
-        for(int i = 0; i < prices.size(); i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
-            }
+    vector<int>min;
+    vector<int>max;
+    int max_diff=INT_MIN;
+    int minimum = prices[0] ;
+    int maximum= prices[prices.size()-1] ;
+    min.push_back(minimum);
+    max.push_back(maximum);
+    for(int i =1;i<prices.size();i++){
+        if(prices[i]<minimum){
+            minimum= prices[i];
         }
-        return op;
+        min.push_back(minimum);
+    }
+    for(int i =prices.size()-2;i>=0;i--){
+        if(prices[i]>maximum){
+           maximum= prices[i];
+        }
+        max.push_back(maximum);
+    }
+    reverse(max.begin(),max.end());
+    for(int j =0;j<max.size();j++){
+        cout<<max[j]<<endl;
+        int a = max[j]-min[j];
+        if(a>max_diff){
+            max_diff=a;
+        }   
+    }
+    
+    return max_diff;
+    
+        
+    
+        
+    
+        
     }
 };
