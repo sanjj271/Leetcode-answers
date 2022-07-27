66279f999dc3316e63d78d128521c04bb954652d
Sanjana gogte <sanj27272@gmail.com>
2022-07-27 14:08:41 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (30.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     map<int,int>mp;
     int climbStairs(int n) {
-    if(n==1){
+    // if(n==0){
+    //     return 0;
+    // }
+    if(n == 1){
         return 1;
     }
     if(n == 2){
         return 2;
     }
-    else{
-        if(mp.find(n)==mp.end()){
-        mp[n] =  climbStairs(n-1) + climbStairs(n-2);
-        }
-        return mp[n];
+    if(mp.find(n)==mp.end()){
+     mp[n]=climbStairs(n-1) + climbStairs(n-2);
     }
-   
-
+    return mp[n];
+    
+        
         
     }
 };
