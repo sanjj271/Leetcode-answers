d7d42b9655fab248c2197599818e4ef3a8537a79
Sanjana gogte <sanj27272@gmail.com>
2022-08-05 08:17:23 +0530

    Time: 23 ms (41.80%), Space: 13 MB (8.15%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,56 @@
+class word{
+    public :
+    int freq;
+    string lexi;
+    word (int f, string l){
+        freq = f;
+        lexi = l;
+        
+    }
+};
+auto it=[]( word a, word b){
+    if(a.freq>b.freq){
+        return false;
+    }
+    else if(a.freq<b.freq){
+        return true;
+    }
+    else 
+    {
+    if (a.lexi<b.lexi){
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+    }
+}; 
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+    priority_queue<word, vector<word>, decltype(it)> pq(it);
+    map<string,int>mp;
+    vector<string>ans;
+    vector<int>v1;
+    int maxval = INT_MIN;
+    for(int i=0;i<words.size();i++){
+        mp[words[i]]++;
+    }
+    for (auto x : mp){
+        word* wp = new word(x.second, x.first);
+        pq.push(*wp);
+    }
+    for(int i =0;i<k;i++){
+        word w= pq.top();
+        ans.push_back(w.lexi);
+        pq.pop();
+    }
+    return ans;
+    
+    
+    
+    
+    }
+    
+};
