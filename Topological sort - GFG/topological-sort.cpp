2891e8c0f473b1326ce179d8a95ebf9754cc5a2a
Sanjana gogte <sanj27272@gmail.com>
2023-01-05 17:36:55 +0530

    Added solution - LeetHub

---
+++ Topological sort - GFG/topological-sort.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>ans;
+	    int count =0;
+	    vector<int>indegree(V,0);
+	    for(int i = 0;i<V;i++){
+	        for(int j =0;j<adj[i].size();j++){
+	            indegree[adj[i][j]]++;
+	        }
+	    }
+	    while(count != V){
+	    for(int k =0;k<indegree.size();k++){
+	        if(indegree[k]==0){
+	            count++;
+	            indegree[k] = -1;
+	            ans.push_back(k);
+	            for(int i =0;i<adj[k].size();i++){
+	                indegree[adj[k][i]]--;
+	            }
+	        }
+	    }
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
