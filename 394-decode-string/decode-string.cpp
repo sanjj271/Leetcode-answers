a89244e78e0838dc592dbfb8a4439a6c15257344
Sanjana gogte <sanj27272@gmail.com>
2023-11-04 23:46:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (16.11%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> countStack;
+        stack<string> stringStack;
+        string currentString;
+        int k = 0;
+        for (auto ch : s) {
+            if (isdigit(ch)) {
+                k = k * 10 + ch - '0';
+            } else if (ch == '[') {
+                // push the number k to countStack
+                countStack.push(k);
+                // push the currentString to stringStack
+                stringStack.push(currentString);
+                // reset currentString and k
+                currentString = "";
+                k = 0;
+            } else if (ch == ']') {
+                string decodedString = stringStack.top();
+                stringStack.pop();
+                // decode currentK[currentString] by appending currentString k times
+                for (int currentK = countStack.top(); currentK > 0; currentK--) {
+                    decodedString = decodedString + currentString;
+                }
+                countStack.pop();
+                currentString = decodedString;
+            } else {
+                currentString = currentString + ch;
+            }
+        }
+        return currentString;
+    }
+};
