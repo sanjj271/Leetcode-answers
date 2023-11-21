6f13218a5f7f9f4dc7272c8641130c055f3929c1
Sanjana gogte <sanj27272@gmail.com>
2023-11-22 01:38:22 +0530

    Time: 3 ms (64.83%) | Memory: 11.4 MB (67.63%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
+    int left =0;
+    int right = nums.size()-1;
+    while(left<=right){
+        int mid = left +(right-left)/2;
+        cout<<nums[mid]<<endl;
+        cout<<nums[left]<<"----"<<nums[right]<<endl;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[left]<=nums[mid]){
+            if(target>=nums[left] && target<nums[mid]){
+                right = mid-1;
             }
-
-            // Check if the left half is sorted
-            if (nums[left] <= nums[mid]) {
-                // Check if the target is in the left half
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+            else{
+                left = mid+1;
             }
-            // Otherwise, the right half must be sorted
-            else {
-                // Check if the target is in the right half
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+        }
+        else if(nums[right]>=nums[mid]){
+            cout<<"here-line22"<<endl;
+            if(target>nums[mid] && target<=nums[right]){
+                cout<<"here-line24"<<endl;
+                left = mid+1;
+            }
+            else{
+                cout<<"here-line28"<<endl;
+                right = mid-1;
             }
         }
 
-        return -1;
+    }
+    return -1;
+
+        
     }
 };
